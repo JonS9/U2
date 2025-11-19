46e9d8c4ed5c76206bc401a95b20e6400cf2a727
JonS9 <jons9@nycstudents.net>
2025-11-19 14:47:26 -0500

    Add files via upload

--- src/ASCIIDEPOSITORY.java
+++ src/ASCIIDEPOSITORY.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ASCIIDEPOSITORY {
     private String type;
     private boolean Sleeping;
@@ -33,5 +38,22 @@ public class ASCIIDEPOSITORY {
 
     public static void PrintModifiedFlower() {
 
+    }
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,29 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        ASCIIDEPOSITORY flower = new ASCIIDEPOSITORY("flower", true, 5, "Bedroom");
-        flower.PrintBasicAscii();
+
+        ArrayList<String> flowerData = ASCIIDEPOSITORY.getFileData("src/flower");
+
+        for (int j = 0; j < 3; j++) {
+            for (int i = 0; i < 5; i++) {
+                System.out.println(flowerData.get(i));
+            }
+        }
+        ArrayList<String> flowerDataBase = ASCIIDEPOSITORY.getFileData("src/flowerbase");
+
+        for (int j = 0; j < 3; j++) {
+            for (int i = 0; i < 5; i++) {
+                System.out.println(flowerData.get(i));
+            }
+        }
+
+
+      ASCIIDEPOSITORY flower = new ASCIIDEPOSITORY("flower", true, 5, "Bedroom");
+      flower.PrintBasicAscii();
     }
+
+
 }
